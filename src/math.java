294baa71ddb3baf45ef716d832bf6aa206edc1dc
xblask04 <xblask04@stud.fit.vutbr.cz>
2017-04-06 12:52:07 +0200

    Written empty math functions with documentation.

---
+++ src/math.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author xblask04
+ */
+public class math {
+    /**
+     * @brief Function will add two numbers.
+     * @param x first number for operation
+     * @param y second number for operation
+     * @return result of adding two parameters
+     */
+    public static double add(double x, double y){
+        return 0.0;
+    }
+    
+    /**
+     * @brief Substraction of two numbers
+     * @param x first number for operation
+     * @param y second number for operation
+     * @return result of substracting two numbers
+     */
+    public static double sub(float x, float y){
+        return 0.0;
+    }
+    
+    /**
+     * @brief Function will multiply two numbers.
+     * @param x first number for operation
+     * @param y second number for operation
+     * @return result of multiplying two numbers
+     */
+    public static double multiply(double x, double y){
+        return 0.0;
+    }
+    
+    /**
+     * @brief Division of two numbers
+     * @param x divident
+     * @param y divisor
+     * @return result of deviding two numbers
+     */
+    public static double divide(double x, double y){
+        return 0.0;
+    }
+    
+    /**
+     * @brief Function will calculate a factorial of a given number.
+     * @param num number from which we want factorial to be calculated from
+     * @return value of the factorial
+     */
+    public static long factorial(long num){
+        return 42;
+    }
+    
+    /**
+     *
+     * @param x floating point base value
+     * @param y power value
+     * @return result of raising x to the power y
+     */
+    public static double pow(double x, int y){
+        return 42;
+    }
+}
