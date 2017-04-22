68faf9c4e747c4e305a73aec6059b364b8168258
xcrkon00 <xcrkon00@stud.fit.vutbr.cz>
2017-04-22 20:24:48 +0200

    Rounding bug fixed, comments added, link css added

--- src/GUI_Controller.java
+++ src/GUI_Controller.java
@@ -1,16 +1,12 @@
 
-
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -31,10 +27,8 @@ public class GUI_Controller implements Initializable {
     private TextField display;
     @FXML
     private TextField OP_display;
-    private double operand_one;
-    private double operand_two;
-    private boolean dot_flag;
-    private boolean reset_D;
+    private double operand_one, operand_two;
+    private boolean dot_flag, reset_D;
     private int operation;
     
     @Override
@@ -49,7 +43,8 @@ public class GUI_Controller implements Initializable {
     @FXML
     /**
      * @brief Zero button pressed.
-     * @param event todo
+     * @param event Represents action of pressing a button
+     * Print zero on display. If reset_D is true also reset display.
      */
     private void zeroAction(ActionEvent event) {
         if(reset_D == true) {
@@ -63,7 +58,8 @@ public class GUI_Controller implements Initializable {
     @FXML
     /**
      * @brief Button '1' pressed.
-     * @param event todo
+     * @param event Represents action of pressing a button
+     * Print one on display. If reset_D is true also reset display.
      */
     private void oneAction(ActionEvent event) {
         if(reset_D == true) {
@@ -77,7 +73,8 @@ public class GUI_Controller implements Initializable {
     @FXML
     /**
      * @brief Button '2' pressed.
-     * @param event todo
+     * @param event Represents action of pressing a button
+     * Print two on display. If reset_D is true also reset display.
      */
     private void twoAction(ActionEvent event) {
         if(reset_D == true) {
@@ -91,7 +88,8 @@ public class GUI_Controller implements Initializable {
     @FXML
     /**
      * @brief Button '3' pressed.
-     * @param event todo
+     * @param event Represents action of pressing a button
+     * Print three on display. If reset_D is true also reset display.
      */
     private void threeAction(ActionEvent event) {
         if(reset_D == true) {
@@ -105,7 +103,8 @@ public class GUI_Controller implements Initializable {
     @FXML
     /**
      * @brief Button '4' pressed.
-     * @param event todo
+     * @param event Represents action of pressing a button
+     * Print four on display. If reset_D is true also reset display.
      */
     private void fourAction(ActionEvent event) {
         if(reset_D == true) {
@@ -119,7 +118,8 @@ public class GUI_Controller implements Initializable {
     @FXML
     /**
      * @brief Button '5' pressed.
-     * @param event todo
+     * @param event Represents action of pressing a button
+     * Print five on display. If reset_D is true also reset display.
      */
     private void fiveAction(ActionEvent event) {
         if(reset_D == true) {
@@ -133,7 +133,8 @@ public class GUI_Controller implements Initializable {
     @FXML
     /**
      * @brief Button '6' pressed.
-     * @param event todo
+     * @param event Represents action of pressing a button
+     * Print six on display. If reset_D is true also reset display.
      */
     private void sixAction(ActionEvent event) {
         if(reset_D == true) {
@@ -147,7 +148,8 @@ public class GUI_Controller implements Initializable {
     @FXML
     /**
      * @brief Button '7' pressed.
-     * @param event todo
+     * @param event Represents action of pressing a button
+     * Print seven on display. If reset_D is true also reset display.
      */
     private void sevenAction(ActionEvent event) {
         if(reset_D == true) {
@@ -161,7 +163,8 @@ public class GUI_Controller implements Initializable {
     @FXML
     /**
      * @brief Button '8' pressed.
-     * @param event todo
+     * @param event Represents action of pressing a button
+     * Print eight on display. If reset_D is true also reset display.
      */
     private void eigthAction(ActionEvent event) {
         if(reset_D == true) {
@@ -175,7 +178,8 @@ public class GUI_Controller implements Initializable {
     @FXML
     /**
      * @brief Button '9' pressed.
-     * @param event todo
+     * @param event Represents action of pressing a button
+     * Print nine on display. If reset_D is true also reset display.
      */
     private void nineAction(ActionEvent event) {
         if(reset_D == true) {
@@ -189,7 +193,8 @@ public class GUI_Controller implements Initializable {
     @FXML
     /**
      * @brief Button 'DEL' pressed.
-     * @param event todo
+     * @param event Represents action of pressing a button
+     * Delete last inserted value on display. If after DEL display would be empty also resets all calculator variables.
      */
     private void DELAction(ActionEvent event) {
         String str = "";
@@ -207,8 +212,9 @@ public class GUI_Controller implements Initializable {
     
     @FXML
     /**
-     * @brief Button 'CA' pressed.
-     * @param event todo
+     * @brief Button 'C' pressed.
+     * @param event Represents action of pressing a button
+     * When button 'C' is pressed calls function reset.
      */
     private void CAction(ActionEvent event) {
         reset();
@@ -217,7 +223,8 @@ public class GUI_Controller implements Initializable {
     @FXML
     /**
      * @brief Button '=' pressed.
-     * @param event todo
+     * @param event Represents action of pressing a button
+     * When button '=' is pressed calls function mid_result().
      */
     private void equalAction(ActionEvent event) {
         mid_result();
@@ -226,7 +233,8 @@ public class GUI_Controller implements Initializable {
     @FXML
     /**
      * @brief Button '.' pressed.
-     * @param event todo
+     * @param event Represents action of pressing a button
+     * Prints dot on display. Only one dot can be displayed.
      */
     private void dotAction(ActionEvent event) {
         if(display.getText().length() > 0 && dot_flag == false && (! display.getText().equals("-"))) {
@@ -238,7 +246,11 @@ public class GUI_Controller implements Initializable {
     @FXML
     /**
      * @brief Button '+' pressed.
-     * @param event todo
+     * @param event Represents action of pressing a button
+     * Function load value on display into operand_one.
+     * Set operation value on 1.
+     * Set reset_D on true.
+     * Set dot_flag on false;
      */
     private void plusAction(ActionEvent event) {
         if(display.getText().length() > 0 && (! display.getText().equals("-"))) {
@@ -247,7 +259,7 @@ public class GUI_Controller implements Initializable {
             }
             dot_flag = false;
             reset_D = true;
-            operand_one = Float.parseFloat(display.getText()); 
+            operand_one = Double.parseDouble(display.getText()); 
             operation = 1; // ADD 
             OP_display.setText("+");
         }
@@ -256,7 +268,12 @@ public class GUI_Controller implements Initializable {
     @FXML
     /**
      * @brief Button '-' pressed.
-     * @param event todo
+     * @param event Represents action of pressing a button
+     * If display length is zero function prints '-' on display if display is empty.
+     * If display length is not zero function load value on display into operand_one.
+     * Set operation value on 3.
+     * Set reset_D on true.
+     * Set dot_flag on false;
      */
     private void minusAction(ActionEvent event) {
 
@@ -270,7 +287,7 @@ public class GUI_Controller implements Initializable {
         else if((display.getText().length() > 1 && display.getText().contains("-")) || (display.getText().length() > 0 && !(display.getText().contains("-")))) {           
             dot_flag = false;
             reset_D = true;
-            operand_one = Float.parseFloat(display.getText()); 
+            operand_one = Double.parseDouble(display.getText()); 
             operation = 3; // SUBSTRACT 
             OP_display.setText("-");
          }
@@ -279,7 +296,11 @@ public class GUI_Controller implements Initializable {
     @FXML
     /**
      * @brief Button '%' pressed.
-     * @param event todo
+     * @param event Represents action of pressing a button
+     * Function load value on display into operand_one.
+     * Set operation value on 6.
+     * Set reset_D on true.
+     * Set dot_flag on false;
      */
     private void moduloAction(ActionEvent event) {
         if(display.getText().length() > 0 && (! display.getText().equals("-"))) {
@@ -288,7 +309,7 @@ public class GUI_Controller implements Initializable {
             }            
             dot_flag = false;
             reset_D = true;
-            operand_one = Float.parseFloat(display.getText()); 
+            operand_one = Double.parseDouble(display.getText()); 
             operation = 6; // MODULO
             OP_display.setText("%");
         }
@@ -297,7 +318,11 @@ public class GUI_Controller implements Initializable {
     @FXML
     /**
      * @brief Button 'sqrt' pressed.
-     * @param event todo
+     * @param event Represents action of pressing a button
+     * Function load value on display into operand_one.
+     * Set operation value on 7.
+     * Set reset_D on true.
+     * Set dot_flag on false;
      */
     private void sqrtAction(ActionEvent event) {
         if(display.getText().length() > 0 && (! display.getText().equals("-"))) {
@@ -306,7 +331,7 @@ public class GUI_Controller implements Initializable {
             }            
             dot_flag = false;
             reset_D = true;
-            operand_one = Float.parseFloat(display.getText()); 
+            operand_one = Double.parseDouble(display.getText()); 
             operation = 7; // sqrt
             OP_display.setText("√");
         }
@@ -315,7 +340,11 @@ public class GUI_Controller implements Initializable {
     @FXML
     /**
      * @brief Button '^' pressed.
-     * @param event todo
+     * @param event Represents action of pressing a button
+     * Function load value on display into operand_one.
+     * Set operation value on 5.
+     * Set reset_D on true.
+     * Set dot_flag on false;
      */
     private void powAction(ActionEvent event) {
         if(display.getText().length() > 0 && (! display.getText().equals("-"))) {
@@ -324,7 +353,7 @@ public class GUI_Controller implements Initializable {
             }            
             dot_flag = false;
             reset_D = true;
-            operand_one = Float.parseFloat(display.getText()); 
+            operand_one = Double.parseDouble(display.getText()); 
             operation = 5; // POW
             OP_display.setText("^");
         }        
@@ -333,7 +362,11 @@ public class GUI_Controller implements Initializable {
     @FXML
     /**
      * @brief Button '*' pressed.
-     * @param event todo
+     * @param event Represents action of pressing a button
+     * Function load value on display into operand_one.
+     * Set operation value on 2.
+     * Set reset_D on true.
+     * Set dot_flag on false;
      */
     private void multiAction(ActionEvent event) {
         if(display.getText().length() > 0 && (! display.getText().equals("-"))) {
@@ -342,7 +375,7 @@ public class GUI_Controller implements Initializable {
             }           
             dot_flag = false;
             reset_D = true;
-            operand_one = Float.parseFloat(display.getText()); 
+            operand_one = Double.parseDouble(display.getText()); 
             operation = 2; // MULTIPLY
             OP_display.setText("*");
         }          
@@ -351,7 +384,11 @@ public class GUI_Controller implements Initializable {
     @FXML
     /**
      * @brief Button '/' pressed.
-     * @param event todo
+     * @param event Represents action of pressing a button
+     * Function load value on display into operand_one.
+     * Set operation value on 4.
+     * Set reset_D on true.
+     * Set dot_flag on false;
      */
     private void divAction(ActionEvent event) {
         if(display.getText().length() > 0 && (! display.getText().equals("-"))) {
@@ -360,7 +397,7 @@ public class GUI_Controller implements Initializable {
             }           
             dot_flag = false;
             reset_D = true;
-            operand_one = Float.parseFloat(display.getText()); 
+            operand_one = Double.parseDouble(display.getText()); 
             operation = 4; // DIVIDE
             OP_display.setText("/");
         } 
@@ -369,13 +406,17 @@ public class GUI_Controller implements Initializable {
     @FXML
     /**
      * @brief Button '!' pressed.
-     * @param event todo
+     * @param event Represents action of pressing a button
+     * Set operation value on 8.
+     * Function load value on display into operand_one and call mid_result().
+     * Set reset_D on true.
+     * Set dot_flag on false; 
      */
     private void factAction(ActionEvent event) {
-        if(display.getText().length() > 0) {        
+        if(display.getText().length() > 0 && (! display.getText().equals("-"))) {        
             dot_flag = false;
             reset_D = true;
-            operand_one = Float.parseFloat(display.getText()); 
+            operand_one = Double.parseDouble(display.getText()); 
             operation = 8;
             mid_result();
         } 
@@ -384,7 +425,8 @@ public class GUI_Controller implements Initializable {
     @FXML
     /**
      * @brief Button '?' pressed.
-     * @param event todo
+     * @param event Represents action of pressing a button
+     * Function opens HELP window.
      */
     private void helpAction(ActionEvent event) {
         try {
@@ -400,11 +442,15 @@ public class GUI_Controller implements Initializable {
           }
     }
     /**
-     * @brief Function handling more than one operation in row. 
+     * @brief Function print result of selected operation on display.
+     * Depending on value in operation, function calls wanted method from math library.
+     * Function use value of operand_one as first operand , load number on display into 
+     * operand_two and use it as second operand for selected operation.
+     * Check if number is integer or floating point number and print value on display.
      */
     private void mid_result() {
         if((operation <= 7) && (operation > 0)) {
-            operand_two = Float.parseFloat(display.getText());
+            operand_two = Double.parseDouble(display.getText());
             double result = 0;
             switch(operation) {
                 case 1: result = math.add(operand_one, operand_two);
@@ -473,7 +519,7 @@ public class GUI_Controller implements Initializable {
         }
     }
     /**
-     * @brief noooo idea
+     * @brief Fucntion resets calculator display and all needed calculator variables
      */
     private void reset() {
         display.setText("");
